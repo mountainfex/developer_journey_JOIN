01b9f34d4576f2e4f6aa8a8be1b219ff48059dd9
Niklas <niklas.tibbe@gmail.com>
2023-03-06 14:22:45 +0100

    contact_animation

--- contact.js
+++ contact.js
@@ -1,56 +1,41 @@
-let openbutton = document.getElementById("openBtn");
-let closebutton = document.querySelector(".xicon");
-let popup = document.querySelector(".popUpBackground");
-let popUpContact = document.querySelector(".popUpContact");
+// let openbutton = document.querySelector(".button");
+// let closebutton = document.querySelector(".xicon");
+// let popup = document.querySelector(".popUpBackground");
+// let popUpContact = document.querySelector(".popUpContact");
 
-openbutton.addEventListener('click', () => {
-	popup.style.display = 'flex';
-	popUpContact.style.cssText = 'animation:slide-in .5s ease; animation-fill-mode: forwards;';
-});
+// openbutton.addEventListener('click', () => {
+//     console.log('test');
+// })
 
-closebutton.addEventListener('click', () => {
-	popUpContact.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
-	setTimeout(() => {
-		popup.style.display = 'none';
-	}, 500);
-});
+// closebutton.addEventListener('click', () => {
+// 	popUpContact.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+// 	setTimeout(() => {
+// 		popup.style.display = 'none';
+// 	}, 500);
+// });
 
-window.addEventListener('click', (e) => {
-	if (e.target == document.querySelector('.popup-overlay')) {
-		popUpContact.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
-		setTimeout(() => {
-			popup.style.display = 'none';
-		}, 500);
-	}
-});
+// window.addEventListener('click', (e) => {
+// 	if (e.target == document.querySelector('.popup-overlay')) {
+// 		popUpContact.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+// 		setTimeout(() => {
+// 			popup.style.display = 'none';
+// 		}, 500);
+// 	}
+// });
 
 // let openbutton = document.getElementById("openBtn");
 // let closebutton = document.getElementById("closeBtn");
-
 // let popUpContact = document.getElementById("mainPopUp");
 
 
-// function slideIn(){
-//     let popup = document.getElementById("popup");
-//     let popUpContact = document.getElementById("mainPopUp");
-//     popUpContact.classList.remove('slideout');
-//     // popup.classList.remove();
-//     // popup.classList.add();
-//     popUpContact.classList.add('slidein');
-// } 
+function slideIn(){
+    let popup = document.getElementById("popup");
+    popup.classList.remove('slideOut');
+    popup.classList.add('slidein');
+} 
 
-// function slideOut() {
-//     let popup = document.getElementById("popup");
-//     popUpContact.classList.remove('slidein');
-//     // popup.classList.remove();
-//     // popup.classList.add();
-//     popUpContact.classList.add('slideout');
-//     setTimeout(popuptimeout, 600);
-
-// }
-
-// function popuptimeout() {
-//     let popUpContact = document.getElementById("mainPopUp");
-//     popUpContact.classList.add('dnone');
-
-// }
+function slideOut() {
+    let popup = document.getElementById("popup");
+    popup.classList.add('slideOut')
+    popup.classList.remove('slidein');
+}
