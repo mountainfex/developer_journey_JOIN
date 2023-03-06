b800017fddc7bc34a6382f3679459f4cf4639626
Niklas <niklas.tibbe@gmail.com>
2023-03-06 12:32:08 +0100

    login_js

---
+++ js/login.js
@@ -0,0 +1,19 @@
+function login (){
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let user = users.find(u => u.email == email.value && u.password == password.value);
+    console.log(user);
+    if (user) {
+        console.log('user gefunden')
+    }
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const msg = urlParams.get('msg');
+let msgBox = document.getElementById("msgBox");
+
+if(msg){
+    msgBox.innerHTML = msg;
+} else {
+    //display:none;
+}
