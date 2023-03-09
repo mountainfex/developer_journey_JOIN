33e0df0f26684d8ae96288e995860511a0b809de
Rafael-Go7 <ra01@outlook.de>
2023-03-09 12:42:30 +0100

    board.js - implementierung - function PopUpContainer_modModus

--- js/board.js
+++ js/board.js
@@ -19,3 +19,56 @@ function slideOut() {
     setTimeout(function(){document.getElementById('popUpContainer').classList.add('dnone')}, 500);
 }
 
+function PopUpContainer_modModus () {
+    Container_modModus = document.getElementById('background');
+
+    Container_modModus.innerHTML = "";
+    Container_modModus.innerHTML = `
+    
+        <div class="PopUpContainerModModus">
+        <img class="xicon" src="assets/img/icon-x.svg" alt="">
+
+        <div class="PopUpMM_Interior">
+            <div class="PopUpModModus_Head">
+            
+                <div class="PopUpMM_title">Title</div>
+                <input id="PopUpMM_title" placeholder="Enter a title">
+                
+                <div class="PopUpMM_Description">Description</div>
+                <textarea id="PopUpMM_Description_textarea" placeholder="Enter a description"></textarea>
+                
+            </div>
+
+            <div class="PopUpModModus_Body">
+                
+                    <div class="PopUpMM_DueDate_1">Due Date</div>
+                    <input type="date" id="PopUpMM_dueDate">
+                                        
+                    <div class="PopUpMM_Priority">Priority</div>
+                    <div class="PopUpMM_Priority_Labels">
+                        
+                        <button onclick="priorityRed()" id="PopUpMM_Priority_Labels_red" class="PopUpMM_Priority_Labels_white">Urgent<img id="urgent_img" src="assets/img/prio-urgent.svg"></button>
+                        <button onclick="priorityYellow()" id="PopUpMM_Priority_Labels_yellow" class="PopUpMM_Priority_Labels_white">Medium<img id="medium_img" src="assets/img/prio-medium.svg"></button>
+                        <button onclick="priorityGreen()" id="PopUpMM_Priority_Labels_green" class="PopUpMM_Priority_Labels_white">Low<img id="low_img" src="assets/img/prio-low.svg"></button>
+                    </div>      
+            </div>
+
+            <div class="PopUpModModus_Bottom">
+                <div class="PopUpMM_Assign_title">Assigned to</div>
+                <select>
+                    <option>Select contacts to assign</option>
+                    <option>David Eisenberg</option>
+                    <option>empty_no Array</option>
+                </select>
+                <div id ="PopUpMM_Assign_PeopleLogo" class="PopUpMM_Assign_PeopleLogo"></div>
+            </div>
+
+            <div>
+                <button class="editbtn"><img src="assets/img/pencil-no-bg.svg" alt=""></button>
+            </div>
+        </div>
+    </div>
+    </div>
+    `
+
+}
