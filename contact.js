fc906500d51e643bf7e2fe34f16c6b7b683c1bd4
CNN87 <125652955+CNN87@users.noreply.github.com>
2023-03-10 12:01:25 +0100

    contacteditsims

--- contact.js
+++ contact.js
@@ -25,6 +25,25 @@ function slideInContact(){
     setTimeout(function(){document.getElementById('contact-selection').classList.remove('slidein')}, 500);
     
 } 
+// popup edit slide
+function slideEditIn(){
+    let popup = document.getElementById("popupedit");
+    let popUpContact = document.getElementById("mainPopUpedit");
+    popUpContact.classList.remove('slideoutE');
+    popUpContact.classList.add('slideinE');
+    popUpContact.classList.remove("dnoneE");
+    popup.classList.remove('fadeoutE');
+    popup.classList.add('fadeinE');
+    popup.classList.remove('dnoneE');
+}
+function slideEditOut() {
+    let popup = document.getElementById("popupedit");
+    let popUpContact = document.getElementById("mainPopUpedit");
+    popUpContact.classList.add('slideoutE');
+    popup.classList.add('fadeoutE');
+    setTimeout(function(){document.getElementById('popupedit').classList.add('dnoneE')}, 500);
+    setTimeout(function(){document.getElementById('mainPopUpedit').classList.add('dnoneE')}, 500);
+}
 
 
 let contacts = [
@@ -33,42 +52,42 @@ let contacts = [
         "firstname": "Anton",
         "surname": "Mayer",
         "email": "antom@gmail.com",
-        "phone": "+49 1111 111 11 1",
+        "phone": "491111111111",
     },
     {
         id: 1,
         "firstname": "Anja",
         "surname": "Schulz",
         "email": "schulz@hotmail.com",
-        "phone": "+49 1111 111 11 1",
+        "phone": "491111111112",
     },
     {
         id: 2,
         "firstname": "Benedikt",
         "surname": "Ziegler",
         "email": "benedikt@gmail.com",
-        "phone": "+49 1111 111 11 1",
+        "phone": "491111111113",
     },
     {
         id: 3,
         "firstname": "David",
         "surname": "Eisenberg",
         "email": "davidberg@gmail.com",
-        "phone": "+49 1111 111 11 1",
+        "phone": "491111111114",
     },
     {
         id: 4,
         "firstname": "Eva",
         "surname": "Fischer",
         "email": "eva@gmail.com",
-        "phone": "+49 1111 111 11 1",
+        "phone": "491111111115",
     },
     {
         id: 5,
         "firstname": "Emmanuel",
         "surname": "Mauer",
         "email": "emmanuelMa@gmail.com",
-        "phone": "+49 1111 111 11 1",
+        "phone": "491111111116",
     }
 ];
 
@@ -135,12 +154,14 @@ function renderContactlist(i, contact, firstletter){
 
 function openContact(i){
     let contact = contacts[i];
-    console.log(contact);
-    console.log("laenge: ", contacts.length);
+    // console.log(contact);
+    // console.log("laenge: ", contacts.length);
     let contactopen = document.getElementById('contact-selection');
     contactopen.innerHTML = "";
     contactopen.innerHTML = renderOpenContact(contact);
+
 }
+
 
 function renderOpenContact(contact){
     return`    
@@ -157,7 +178,7 @@ function renderOpenContact(contact){
                     </div>
                     <div class="contact-information-edit">
                         <span>Contact Information</span>
-                        <div class="contact-information-edit-button" id="contact-information-edit-button" onclick="contact-information-edit-button(${contact["id"]})">
+                        <div class="contact-information-edit-button" id="contact-information-edit-button" onclick="contactInformationEditButton(${contact["id"]}), slideEditIn()">
                             <img src="assets/img/pencil-no-bg.svg">
                             <span>Edit Contact</span>
                         </div>
@@ -194,4 +215,87 @@ function addNewContact(){
     console.log(contacts);
     contactinit();
     
+}
+// edit contact
+function contactInformationEditButton(i){
+    let popupedit = document.getElementById('popupedit');
+    popupedit.innerHTML = "";
+    popupedit.innerHTML = renderPopupEditContact(i);
+    console.log(i);
+
+    let firstname = contacts[i]["firstname"];
+    let surname = contacts[i]["surname"];
+    let fullsname = firstname + " " + surname; 
+    let email = contacts[i]["email"];
+    let phone = contacts[i]["phone"];
+    document.getElementById(`editName${i}`).value = fullsname;
+    document.getElementById(`editEmail${i}`).value = email;
+    document.getElementById(`editPhone${i}`).value = phone;
+}
+function renderPopupEditContact(i){
+    return`
+    <div class="popUpContactE" id="mainPopUpedit">
+        <div class="popUpLeftE">
+            <img class="logoimgE" src="assets/img/logo_light.png" alt="">
+            <div class="popUpLeftTextE">
+                <p class="popUpLeftHeaderE">Edit contact</p>
+            </div>
+            <img src="assets/img/Vector 5 (1).png" alt="">
+        </div>
+        <div class="popUpRightE">
+            <img class="xiconE" id="closeBtnE" onclick="slideEditOut()" src="assets/img/icon-x.svg" alt="">
+            <div class="popUpRightImgE" id="popupEditInitials">
+                <div class="popUpRightImgBGE">${contacts[i]["firstname"].charAt(0)}${contacts[i]["surname"].charAt(0)}
+                    
+                    
+                </div>                    
+            </div>
+            <form onsubmit="saveEditContact(${i}); return false" class="popUpAddContactE">
+                <div class="inputE">
+                    <input required type="text" placeholder="Name" id="editName${i}">
+                    <img src="assets/img/icon-user-grey.svg" alt="">
+                </div>
+                <div class="inputE">
+                    <input required type="email" placeholder="Email" id="editEmail${i}">
+                    <img src="assets/img/email_input.svg" alt="">
+                </div>
+                <div class="inputE">
+                    <input required type="number" placeholder="Phone" id="editPhone${i}">
+                    <img src="assets/img/icon-phone.svg" alt="">
+                </div>
+                <div class="popUpButtonE">
+                    <button class="saveBtnE buttonE" onclick="saveEditContact(${i})">Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+    `;
+}
+function saveEditContact(i){
+    let name = document.getElementById(`editName${i}`).value;
+    let email = document.getElementById(`editEmail${i}`).value;
+    let phone = document.getElementById(`editPhone${i}`).value;
+    console.log("name: ",name);
+    console.log("email: ",email);
+    console.log("phone: ",phone);
+
+
+    fullname = name.split(' ');
+    firstn = fullname[0];
+    lastn = fullname[1];
+    let firstn2 = firstn.charAt(0).toUpperCase() + firstn.slice(1);
+    let lastn2 = lastn.charAt(0).toUpperCase() + lastn.slice(1);
+
+    
+    let newcontact = {
+        id: i,
+        "firstname": firstn2,
+        "surname": lastn2,
+        "email": email,
+        "phone": phone,
+    }
+    contacts.splice(i, 1, newcontact);
+    console.log(contacts);
+    contactinit();
 }
