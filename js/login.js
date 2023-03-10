1316c9c2d997fc8b94eff83afd9ea029d5e3f1c4
Niklas <niklas.tibbe@gmail.com>
2023-03-10 09:02:37 +0100

    remember_me

--- js/login.js
+++ js/login.js
@@ -33,3 +33,24 @@ function shakeElement() {
 
 // let msgBox = document.getElementById('msgBox');
 // setTimeout(function(){msgBox.classList.add('dnone')}, 2000);
+
+const rmCheck = document.getElementById("rememberMe"),
+    emailInput = document.getElementById("email");
+
+if (localStorage.checkbox && localStorage.checkbox !== "") {
+  rmCheck.setAttribute("checked", "checked");
+  emailInput.value = localStorage.username;
+} else {
+  rmCheck.removeAttribute("checked");
+  emailInput.value = "";
+}
+
+function lsRememberMe() {
+  if (rmCheck.checked && emailInput.value !== "") {
+    localStorage.username = emailInput.value;
+    localStorage.checkbox = rmCheck.value;
+  } else {
+    localStorage.username = "";
+    localStorage.checkbox = "";
+  }
+}
